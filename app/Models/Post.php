4bdcb97e6793c20d5366b04c7f73ecc5f5d927bd
Heruvim6w <heruvim.6w@gmail.com>
2023-09-08 20:21:18 +0300

    Связал категории с постами

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
@@ -26,5 +27,10 @@ class Post extends Model
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return  $this->belongsToMany(Category::class);
     }
 }
